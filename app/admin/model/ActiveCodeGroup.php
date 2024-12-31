71fded8bcaee82786abf73794035f26be2a64888
lurui <muscle_rui@163.com>
2024-12-31 17:55:15 +0800

    new

--- app/admin/model/ActiveCodeGroup.php
+++ app/admin/model/ActiveCodeGroup.php
@@ -9,9 +9,12 @@ use app\common\model\BaseModel;
 class ActiveCodeGroup extends BaseModel
 {
 
-     protected string $selectField = 'id,merchant_id,active_code,group_name,status,remark,create_time';
+     protected string $selectField = 'id,merchant_id,group_name,status,remark,create_time,merchant.merchant_name,active_code.active_code';
 
-      protected array $searchField = ['merchant_id','active_code','group_name','status'];
+      protected array $searchField = ['merchant_id','group_name','status','merchant.merchant_name|like','active_code.active_code'];
 
-    
+    protected array $join = [
+        ['merchant' , 'merchant.id = ActiveCodeGroup.merchant_id'],
+        ['active_code' , 'ActiveCodeGroup.id = active_code.active_code_group_id']
+    ];
 }
