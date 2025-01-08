555473aefb94e1039ec1024ed305d31699467967
lurui <muscle_rui@163.com>
2025-01-08 16:52:49 +0800

    new

--- app/api/controller/WorkOrderAccount.php
+++ app/api/controller/WorkOrderAccount.php
@@ -61,7 +61,7 @@ class WorkOrderAccount extends Base
 //        $last_login_time = $params['last_login_time'] ?? date('Y-m-d H:i:s'); // 下线时间
         $sessionId = $params['sessionId'] ?? []; // 会话id
         $order_code = $params['order_number'] ?? '';
-        trace($sessionId, 'log'.'批量下线');
+        trace(json_encode($sessionId), 'log'.'批量下线');
         if (!$sessionId) {
             return $this->success();
         }
